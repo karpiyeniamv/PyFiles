afac30ba3cf1f2d3f4b428cb495f98db9d48ee16
KarpiyeniaM <karpiyenia.mv@gmail.com>
2017-11-03 19:56:35 +0300

    change alg about big nums

--- five_pal.py
+++ five_pal.py
@@ -17,82 +17,33 @@ def is_palindrome(word):
 
 #print (str(10000*10000)+"-"+str (99999*99999))
 
-def func (a):
+def func (a,mn1,mn2):
     flag=False
-    tempMin=0
-    tempMax=0
-    b=tempMax
-    while (flag==False & b>=tempMin):
-
-
-
+    b=99999
+    while ( flag==False and b>10000):
+        if (prime(b)==1):
+            print(str(b)+" "+str(a%b))
+            if ((a%b)==0):
+                if (prime(a/b)==1):
+                    mn1=b
+                    mn2=a/b
+                    return 1
+            if (b<(a/b)):
+                flag=True
+                return 0
+        b=b-1
 
 mn1=1
 mn2=1
-a=9999799999
+a=9999999999
 while (a>9000000008):
     if (is_palindrome(str(a))==True):
-
-
         print(str(a))
+        if (func(a,mn1,mn2)==1):
+            print(str(a)+" "+str(mn1)+" "+str(mn2))
+            a=1
     a=a-1
 
 
 
-# a=9999799999
-# while (a>9000000008):
-#     if (is_palindrome(str(a))==True):
-#         print(str(a))
-#     a=a-1
 
-# a=10000
-# count=0
-# while (a<99999):
-#     if (prime(a)==1):
-#         print(str(a))
-#         count=count+1
-#     a=a+1
-# print (str(count))
-
-
-
-# a=100000000
-# count=0
-# while (a<9999800001):
-#     if (is_palindrome(str(a))==True):
-#         print(a)
-#         count=count+1
-#     a=a+1
-#
-# print ("all count: "+str(count))
-
-
-# a=99999
-# flag=0
-# max=1
-# maxA=1
-# maxB=1
-# b=a
-# while(a>9000):
-#     if (prime(a)==1):
-#         print(a)
-#         while (b>9000):
-#             if (prime(b)==1):
-#                 temp=a*b
-#                 if (temp>max):
-#                     if (is_palindrome(str(temp))==True):
-#                         print(temp)
-#                         maxA=a
-#                         maxB=b
-#                         max=temp
-#             b=b-1
-#     a=a-1
-#     b=a
-#
-# print (str(max)+"="+str(maxA)+"*"+str(maxB))
-
-
-
-
-
-##проверка на простоту
